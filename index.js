3bfcddca3e18b2996300e78f14a5ae97716ca69b
Tony <thunter1987@yahoo.com>
2023-04-12 17:48:20 -0500

    'tests passed"

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+
+  function superbowlWin(record) { 
+    const foundYear = record.find(({result}) => result === 'W')
+    if (foundYear){
+  return foundYear.year
+    }
+    else{
+      }
+  }
